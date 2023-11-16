a888c3f8db17b46ad2f116a056faa97fbc7807d3
MrRadm1r <rarmir1200363rus@gmail.com>
2023-11-16 19:08:41 +0500

    translating

--- Main.py
+++ Main.py
@@ -1,35 +1,41 @@
 import pygame as pg
 import ctypes
-import pygame
+from sprite import Sprite
 
-window = ctypes.windll.user32
-x = window.GetSystemMetrics(0) * 0.55
-y = window.GetSystemMetrics(1) * 0.55
 
 class Jersey:
+    pg.init()  
     def __init__(self) -> None:
-        pygame.init()  
         window = ctypes.windll.user32
-        x = window.GetSystemMetrics(0) * 0.55
-        y = window.GetSystemMetrics(1) * 0.55
-        self.win = pg.display.set_mode((x, y)) 
-        self.posx = 50
-        self.posy = 50   
-        self.width = 40 
-        self.height = 60  
-        self.speed = 4 
+        x = window.GetSystemMetrics(0)
+        y = window.GetSystemMetrics(1)
+        self.win = pg.display.set_mode((x, y)) # Настройка размера окна
         self.run = True
+        self.fps = 60
+        self.clock = pg.time.Clock()
+        # Загрузка изображений
+        self.bg = pg.image.load('img/bg.png').convert()
+        # конец
+        # Писать до
         self.game()
-    def game(self):
+
+
+    def game(self) -> None:
         while self.run:
-            pygame.time.delay(50)
+            
+
+            self.drawon()
+            # проверка ивентов
             for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    run = False
-            self.win.fill((0,0,0))
-            pygame.draw.rect(self.win, (123, 123, 255), (self.posx, self.posy, self.width, self.height))
-            pygame.display.update()
-        pygame.quit()
+                if event.type == pg.QUIT: # Завершение программы
+                    self.run = False
+            self.clock.tick(self.fps)
+        pg.quit()
+
+    def drawon(self):
+        self.win.blit(self.bg, (0, 0))
+        Sprite(self.win, 0, 0, "img/asteroid_1.png")
+        pg.display.update()
 
 
 Jersey()
