c9d3ca3d2e24edd5b65ad873d670d7827356ef08
MrRadm1r <rarmir1200363rus@gmail.com>
2023-11-17 23:11:44 +0500

    imp

--- sprite.py
+++ sprite.py
@@ -1,17 +1,20 @@
 import pygame as pg
 
+fps = 72
+
 class Sprite(pg.sprite.Sprite):
     def __init__(self, images):
         super().__init__()
         self.frame = 0
         self.images = images
         self.image = self.images[self.frame]
+        self.lenght = len(self.images)
         self.rect = self.image.get_rect(topleft=(0, 0))
-        self.animation_timer = 0
+        # self.animation_timer = 0
 
     def update(self, ticks):
-        if ticks % 24 == 0:
-            self.frame = (self.frame + 1) % len(self.images)
+        if ticks % (fps // self.lenght) == 0:
+            self.frame = (self.frame + 1) % self.lenght
             self.image = self.images[self.frame]
 
     def move(self, c: complex, position="topleft"):
