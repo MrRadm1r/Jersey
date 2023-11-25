2872d5f700244e436859ee7aaff5f53ddbf01ce1
MrRadm1r <rarmir1200363rus@gmail.com>
2023-11-25 16:09:19 +0500

    imp

--- tests/complex.c
+++ tests/complex.c
@@ -1,6 +1,12 @@
-#include <complex.h>
+//#include <complex.h>
+//
+//double complex calculate_roots(double complex z) {
+//    const double sqrt2 = 1.414;  // Константа корня из двух
+//    return z / sqrt2;
+//}
 
-double complex calculate_roots(double complex z) {
-    const double sqrt2 = 1.414;  // Константа корня из двух
-    return z / sqrt2;
+#include <stdio.h>
+
+void main() {
+    printf("HI");
 }
--- tests/complex.py
+++ tests/complex.py
@@ -1,5 +1,10 @@
-def ns(c):
-    if c == 0:
-     return 0
-    abs_c = abs(c)
-    return complex(c.real**2/abs_c, c.imag**2/abs_c)
+# def ns(c):
+#     if c == 0:
+#      return 0
+#     abs_c = abs(c)
+#     return complex(c.real**2/abs_c, c.imag**2/abs_c)
+
+import ctypes
+
+c = ctypes.CDLL("C:/Users/1/AppData/Local/Programs/Jersey/tests/complex.dll")
+c.main()
